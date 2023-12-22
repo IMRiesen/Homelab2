6442348a814b240509e5afc8ac41d90c591850e3
IMRiesen <79581574+IMRiesen@users.noreply.github.com>
2023-12-22 23:53:57 +0300

    Add files via upload

---
+++ zad1.pas
@@ -0,0 +1,20 @@
+﻿program zad2;
+var
+  inputString: string;
+  uniqueSubstrings: set of string;
+  i: integer;
+  substring: string;
+begin
+  uniqueSubstrings := [];
+  writeln('Введите строку: ');
+  readln(inputString);
+  for i := 1 to length(inputString) - 2 do
+  begin
+    substring := inputString[i] + inputString[i+1] + inputString[i+2];
+    uniqueSubstrings := uniqueSubstrings + [substring];
+  end;
+  writeln('Количество уникальных подстрок из трех символов: ', uniqueSubstrings);
+end.
+
+
+
